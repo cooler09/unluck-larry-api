e728ab6dafbe94b2215c0de751cefc37e60638c2
Zachary Lockhart <zacharyjordan82@gmail.com>
2018-06-28 18:39:08 -0400

    stuff

---
+++ Controllers/GameStateController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using unlucky_larry.Models;
+
+namespace unlucky_larry.Controllers
+{
+    [Route("api/[controller]")]
+    public class GameStateController:Controller
+    {
+        private readonly QuestionContext _context;
+        public GameStateController(QuestionContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public GameStateData Get()
+        {
+            var user = new User();
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            var model = new GameStateData
+            {
+                UserId = user.Id
+            };
+            return model;
+        }
+    }
+}
---
+++ Models/GameStateData.cs
@@ -0,0 +1,7 @@
+﻿namespace unlucky_larry.Models
+{
+    public class GameStateData
+    {
+        public int UserId { get; set; }
+    }
+}
